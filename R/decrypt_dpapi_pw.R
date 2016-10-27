33eaaa0c786a4986a4908e9784a19e48f612e396
Josh Gilfillan <joshua.i.gilfillan+keyringr@gmail.com>
2016-10-27 09:21:32 +0000

    version 0.2.0

--- R/decrypt_dpapi_pw.R
+++ R/decrypt_dpapi_pw.R
@@ -5,8 +5,28 @@
 #' variable or used inline.
 #' @details
 #' Requires Powershell to be installed and execution policy set to RemoteSigned.
+#' This can be achieved by running
+#' \code{
+#' Set-ExecutionPolicy RemoteSigned
+#' }
+#' from Powershell.
+#' @examples
+#' \dontrun{
+#' # First run the command below from Powershell:
+#' # Read-Host "PW?" -AsSecureString |  ConvertFrom-SecureString | Out-File "C:\Temp\Password.txt"
+#' # Now execute the following R commands to decrypt the password and save it in
+#' # variable "x".  Note that if run without assignment, the password will not
+#' # be displayed in the console.  Passwords must be saved to a variable or used
+#' # inline within a connection string.
+#' library("keyringr")
+#' x <- decrypt_dpapi_pw("C:\\Temp\\Password.txt")
+#'
+#' # function is best used in a connection string command:
+#' ch <- odbcConnect("some dsn", uid = "user1", pwd = decrypt_dpapi_pw("C:\\Temp\\Password.txt"))
+#' }
 #' @export
 decrypt_dpapi_pw <- function(file) {
+  # Test OS is Windows
   stopifnot(.Platform$OS.type == "windows")
   if (missing(file)) {
     warning("encrypted file argument must be defined")
@@ -14,6 +34,6 @@ decrypt_dpapi_pw <- function(file) {
     # construct command
     command <- paste('powershell -command "$PlainPassword = Get-Content ', file, '; $SecurePassword = ConvertTo-SecureString $PlainPassword; $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword); $UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR); echo $UnsecurePassword"', sep='')
     # execute powershell and return command
-    return(invisible(system(command, intern=TRUE)))
+    invisible(system(command, intern=TRUE))
   }
 }
--- R/decrypt_gk_pw.R
+++ R/decrypt_gk_pw.R
@@ -3,14 +3,23 @@
 #' @return An decrypted password as an invisible string.  Invisible means that
 #' the password won't be displayed in the console, but can be assigned to a
 #' variable or used inline.
+#' @details
+#' Requires the Gnome Keyring and secret-tool to be installed.
 #' @examples
 #' \dontrun{
-#' # returns the password for the key-value pair key: MYSCHEMA_MYDB
-#' decrypt_gk_pw("key MYSCHEMA_MYDB")
+#' # First encrypt a password using secret-tool as follows:
+#' # secret-tool store --label=mylabel db mydb user user1
+#' # now return the password above to the R environment
+#' x <- decrypt_gk_pw("db mydb user user1")
+#'
+#' # function is best used in a connection string command:
+#' ch <- odbcConnect("some dsn", uid = "user1", pwd = decrypt_gk_pw("db mydb user user1"))
 #' }
 #' @export
 decrypt_gk_pw <- function(key_value_pairs) {
+  # test OS is Linux
   stopifnot(Sys.info()["sysname"] == "Linux")
-  return(invisible(system(paste("secret-tool lookup ", key_value_pairs, sep=""))))
-}
+  # execute secret-tool command and invisible return result
+  invisible(system(paste("secret-tool lookup ", key_value_pairs, sep=""), intern=TRUE))
+  }
 
